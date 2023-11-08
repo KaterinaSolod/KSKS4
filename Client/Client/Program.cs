8d50a9b31cd965af15cd0a5dee277db900baf693
JuliaS <JuliaS@DESKTOP-KKKBFA0>
2023-11-08 11:42:43 +0200

    Добавьте файлы проекта.

---
+++ Client/Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Labk2Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverIP = "127.0.0.1";
+            int serverPort = 12345;
+
+            using (UdpClient udpClient = new UdpClient())
+            {
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+
+                while (true)
+                {
+                    Console.WriteLine("Введіть команду:");
+                    Console.WriteLine("1. Отримати час");
+                    Console.WriteLine("2. Вийти");
+                    Console.Write("Введіть номер команди: ");
+
+                    string userInput = Console.ReadLine();
+
+                    if (userInput == "2")
+                    {
+                        break;
+                    }
+                    else if (userInput == "1")
+                    {
+                        string currentTime = DateTime.Now.ToString("HH:mm:ss");
+
+                        string message = "get time:" + currentTime;
+                        byte[] data = Encoding.UTF8.GetBytes(message);
+
+                        try
+                        {
+                            udpClient.Send(data, data.Length, serverEndPoint);
+                            Console.WriteLine("Час відправлено на сервер: " + currentTime);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Помилка при відправці часу на сервер: " + e.Message);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Невідома команда. Виберіть 1 або 2.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Labk2/Labk2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Labk2
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udpListener;
+        private Thread listenThread;
+        private System.Windows.Forms.Timer timer;
+        private string currentTime = "00:00:00";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeUdpListener();
+            InitializeTimer();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void InitializeUdpListener()
+        {
+            udpListener = new UdpClient(12345);
+
+            listenThread = new Thread(new ThreadStart(ListenForData));
+            listenThread.IsBackground = true;
+            listenThread.Start();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTimeOnForm(currentTime);
+            DateTime dt = DateTime.ParseExact(currentTime, "HH:mm:ss", null);
+            dt = dt.AddSeconds(1);
+            currentTime = dt.ToString("HH:mm:ss");
+        }
+
+        private void ListenForData()
+        {
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 12345);
+                    byte[] data = udpListener.Receive(ref endPoint);
+                    string message = Encoding.UTF8.GetString(data);
+
+                    if (message.StartsWith("get time:"))
+                    {
+                        currentTime = message.Substring("get time:".Length);
+                        UpdateTimeOnForm(currentTime);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неизвестна команда: " + message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Помилка при обробці даних: " + ex.Message);
+                }
+            }
+        }
+
+        private void UpdateTimeOnForm(string time)
+        {
+            if (labelTime.InvokeRequired)
+            {
+                labelTime.Invoke(new Action(() => labelTime.Text = "Час: " + time));
+            }
+            else
+            {
+                labelTime.Text = "Час: " + time;
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            udpListener.Close();
+            listenThread.Abort();
+        }
+    }
+}
